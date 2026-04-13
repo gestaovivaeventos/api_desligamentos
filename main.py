c033146c971fafabaa9112c4b56c92ef41b4513d
gestaovivaeventos <gestao@vivaeventos.com.br>
2026-04-13 15:10:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -45,53 +45,77 @@ def obter_dados(limit: int = 5000, offset: int = 0):
         conn = pool.getconn()
         with conn.cursor() as cursor:
             query = """
-                SELECT
-                    u.id,
-                    CASE
-                        WHEN uni.id = 31 THEN 'Itaperuna Muriae'
-                        ELSE uni.nm_unidade
-                    END AS nm_unidade,
-                    CASE
-                        WHEN uni_main.id = 31 THEN 'Itaperuna Muriae'
-                        ELSE uni_main.nm_unidade
-                    END AS nm_unidade_principal_desc,
-                    u.nome,
-                    u.username,
-                    u.enabled,
-                    uni.id AS id_unidade,
-                    uv.prioritaria AS unidade_principal,
-                    gr.name AS "nm_grupo/cargo",
-                    u.datacriacao AS dt_criacao,
-                    u.last_login AS ultimo_acesso
-                FROM
-                    tb_usuario u
-                    LEFT JOIN tb_unidade_vinculada uv ON uv.id_usuario = u.id
-                    LEFT JOIN tb_unidade uni ON uni.id = uv.id_unidade
-                    LEFT JOIN tb_grupo gr ON gr.id = uv.id_grupo
-                    JOIN tb_grupo_permissao per ON per.id_grupo = gr.id
-                    LEFT JOIN tb_setor s ON s.id = gr.id_setor
-                    LEFT JOIN tb_unidade_vinculada uv_main ON uv_main.id_usuario = u.id
-                    AND uv_main.prioritaria IS TRUE
-                    LEFT JOIN tb_unidade uni_main ON uni_main.id = uv_main.id_unidade
-                WHERE
-                    -- CORREÇÃO ABAIXO: Uso de %% para escapar a porcentagem
-                    gr.name NOT LIKE '%%Integrantes%%'
-                    AND gr.name NOT LIKE '%%Comissão%%'
-                    AND u.cpf IS NULL
-                    AND uni.id NOT IN (8,7,43,49,69,75,50,79,51,61,73,47,48,37)
-                    AND uni.fl_ativa
-                GROUP BY
-                    u.username,
-                    uni.nm_unidade,
-                    u.id,
-                    uni.id,
-                    s.setor,
-                    gr.name,
-                    uv.prioritaria,
-                    uni_main.id,
-                    uni_main.nm_unidade
-                ORDER BY
-                    u.id
+                SELECT 
+    CASE
+        WHEN u.nm_unidade = 'Campos' THEN 'Itaperuna Muriae'
+        ELSE u.nm_unidade
+    END AS nm_unidade, 
+    f.id AS id_fundo,
+    f.nm_fundo, 
+    -- SITUAÇÃO DO FUNDO 
+    CASE
+        WHEN f.situacao = 1 THEN 'Não mapeado'
+        WHEN f.situacao = 2 THEN 'Mapeado'
+        WHEN f.situacao = 3 THEN 'Em Negociação'
+        WHEN f.situacao = 4 THEN 'Concorrente'
+        WHEN f.situacao = 5 THEN 'Comum'
+        WHEN f.situacao = 6 THEN 'Juntando'
+        WHEN f.situacao = 7 THEN 'Junção'
+        WHEN f.situacao = 8 THEN 'Unificando'
+        WHEN f.situacao = 9 THEN 'Unificado'
+        WHEN f.situacao = 10 THEN 'Rescindindo'
+        WHEN f.situacao = 11 THEN 'Rescindido'
+        WHEN f.situacao = 12 THEN 'Realizado'
+        WHEN f.situacao = 13 THEN 'Desistente'
+        WHEN f.situacao = 14 THEN 'Pendente'
+        ELSE 'NÃO MAPEADO' 
+    END AS situacao_fundo,
+    -- SERVIÇO DO FUNDO
+    CASE 
+        WHEN f.tp_servico = 1 THEN 'Pacote'
+        WHEN f.tp_servico = 2 THEN 'Assessoria'
+        WHEN f.tp_servico = 3 THEN 'Super Integrada'
+    END AS tp_servico,
+    TO_CHAR(f.dt_cadastro, 'YYYY-MM-DD') AS dt_cadastro_fundo,
+    TO_CHAR(f.dt_baile, 'YYYY-MM-DD') AS dt_baile,
+    c.nm_curso,    
+    i.id AS id_integrante,
+    i.nm_integrante,
+    CASE
+        WHEN i.nu_status = 2 THEN 'Comum'
+        WHEN i.nu_status = 4 THEN 'Desligado'
+        WHEN i.nu_status = 5 THEN 'Desligado Permuta'
+        WHEN i.nu_status = 6 THEN 'Desligado Automático'
+        WHEN i.nu_status = 10 THEN 'Integrante reativado'
+        WHEN i.nu_status = 9 THEN 'Integrante fundo'
+        WHEN i.nu_status = 8 THEN 'Cadastro errado'
+        WHEN i.nu_status = 7 THEN 'Formado'
+        WHEN i.nu_status = 13 THEN 'Temporário'
+        WHEN i.nu_status = 14 THEN 'Migração de fundos'
+        WHEN i.nu_status = 15 THEN 'Integrante isento'
+    END AS situacao_atual_integrante,
+    TO_CHAR(i.dt_cadastro, 'YYYY-MM-DD') AS dt_cadastro_integrante,
+    TO_CHAR(i.dt_desligamento, 'YYYY-MM-DD') AS dt_desligamento_integrante,
+    fc.vl_plano AS vl_plano
+FROM 
+    tb_integrante i
+    JOIN tb_fundo f ON f.id = i.fundo_id
+    JOIN tb_unidade u ON u.id = f.unidade_id
+    JOIN tb_curso c ON c.id = f.curso_id
+    LEFT JOIN tb_fundo_cota fc ON fc.cota_id = i.cota_id
+    AND i.fundo_id = fc.FUNDO_ID
+WHERE 
+    i.fl_ativo = false 
+    AND i.dt_desligamento IS NOT NULL
+    AND i.dt_desligamento < f.dt_baile
+    AND f.tipocliente_id = '15' -- FUNDO DE FORMATURA
+    AND f.is_fundo_teste is false
+    AND i.dt_desligamento > '2018-12-31'
+    AND u.categoria = '2' -- FRANQUIA VIVA EVENTOS    
+    AND i.nu_status NOT IN (5, 7, 8, 9, 12, 13, 14) -- DESLIGADO PERMUTA, FORMADO, CADASTRO ERRADO, INTEGRANTE FUNDO, TEMPORÁRIO, MIGRAÇÃO DE FUNDOS
+ORDER BY 
+    i.dt_desligamento
+
                 LIMIT %s OFFSET %s
             """
             cursor.execute(query, (limit, offset))
